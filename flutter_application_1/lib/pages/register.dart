6dfb66f42c2c570ba2426e158776c2b8da5237c9
spirosspiros <spiros@fake.com>
2022-03-08 01:33:25 +0200

    checkbox in regi

--- flutter_application_1/lib/pages/register.dart
+++ flutter_application_1/lib/pages/register.dart
@@ -2,6 +2,43 @@ import 'dart:html';
 
 import 'package:flutter/material.dart';
 
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return Colors.blue;
+      }
+      return Colors.red;
+    }
+
+    return Checkbox(
+      checkColor: Colors.white,
+      fillColor: MaterialStateProperty.resolveWith(getColor),
+      value: isChecked,
+      onChanged: (bool? value) {
+        setState(() {
+          isChecked = value!;
+        });
+      },
+    );
+  }
+}
+
 class Register extends StatefulWidget {
   @override
   _RegisterState createState() => _RegisterState();
@@ -62,6 +99,7 @@ class _RegisterState extends State<Register> {
             ),
           ),
         ),
+        MyStatefulWidget(),
         Padding(
           padding: const EdgeInsets.all(38.0),
           child: Builder(builder: (context) {
