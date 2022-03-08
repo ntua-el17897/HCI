9d8d97db588504cee536bcc511e14ef3e2ab1303
spiros <me@me.me>
2022-03-09 01:29:27 +0200

    photos added

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -34,9 +34,7 @@ class MyApp extends StatelessWidget {
         body: Container(
           decoration: const BoxDecoration(
             image: DecorationImage(
-                image: NetworkImage(
-                    "https://images.unsplash.com/photo-1641451029786-9caca048d9af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"),
-                fit: BoxFit.fill),
+                image: AssetImage('../assets/kourtina.jpg'), fit: BoxFit.fill),
           ),
           child: Container(
             child: SizedBox.expand(
