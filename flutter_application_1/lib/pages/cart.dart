f8e132f5d80795f17dd246bdbef8c1846b78107b
spiros <me@me.me>
2022-03-10 13:53:05 +0200

    now the navbar open a widget with the coresponding name

--- flutter_application_1/lib/pages/cart.dart
+++ flutter_application_1/lib/pages/cart.dart
@@ -11,7 +11,72 @@ class _CartState extends State<Cart> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Text('cart'),
+      body: MyCartWidget(),
+    );
+  }
+}
+
+class MyCartWidget extends StatefulWidget {
+  const MyCartWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyCartWidget> createState() => _MyCartWidgetState();
+}
+
+class _MyCartWidgetState extends State<MyCartWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: cart',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: cart',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: cart',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: const Text('BottomNavigationBar Sample'),
+      // ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.deepPurpleAccent,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu_rounded),
+            label: 'Menu',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
--- flutter_application_1/lib/pages/productsOverview.dart
+++ flutter_application_1/lib/pages/productsOverview.dart
@@ -1,14 +1,8 @@
 import 'dart:html';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pages/register.dart';
 
 import 'cart.dart';
-
-class ProductsOverview extends StatefulWidget {
-  @override
-  _ProductsOverviewState createState() => _ProductsOverviewState();
-}
 
 class _ProductsOverviewState extends State<ProductsOverview> {
   static const String _title = 'Flutter Code Sample';
@@ -29,27 +23,14 @@ class _ProductsOverviewState extends State<ProductsOverview> {
   }
 }
 
-class MyNavBarWidget extends StatefulWidget {
-  const MyNavBarWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyNavBarWidget> createState() => _MyNavBarWidgetState();
-}
-
 class _MyNavBarWidgetState extends State<MyNavBarWidget> {
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Menu',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Home',
-      style: optionStyle,
-    ),
-    MyNavBarWidget()
+    MyMenuWidget(),
+    MyHomeWidget(),
+    MyCartWidget()
   ];
 
   void _onItemTapped(int index) {
@@ -89,4 +70,146 @@ class _MyNavBarWidgetState extends State<MyNavBarWidget> {
       ),
     );
   }
+}
+
+class _MyHomeWidgetState extends State<MyHomeWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: hmomeMenu',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1:home Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1:home Home',
+      style: optionStyle,
+    )
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: const Text('BottomNavigationBar Sample'),
+      // ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.deepPurpleAccent,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu_rounded),
+            label: 'Menu',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class _MyMenuWidgetState extends State<MyMenuWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: menu',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1:menumenu',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1:menu menu',
+      style: optionStyle,
+    )
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: const Text('BottomNavigationBar Sample'),
+      // ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.deepPurpleAccent,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu_rounded),
+            label: 'Menu',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class MyNavBarWidget extends StatefulWidget {
+  const MyNavBarWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyNavBarWidget> createState() => _MyNavBarWidgetState();
+}
+
+class MyMenuWidget extends StatefulWidget {
+  const MyMenuWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyMenuWidget> createState() => _MyMenuWidgetState();
+}
+
+class MyHomeWidget extends StatefulWidget {
+  const MyHomeWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomeWidget> createState() => _MyHomeWidgetState();
+}
+
+class ProductsOverview extends StatefulWidget {
+  @override
+  _ProductsOverviewState createState() => _ProductsOverviewState();
 }
